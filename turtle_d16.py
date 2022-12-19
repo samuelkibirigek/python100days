118b57026bf18346d885b36c2ebfe8300e91b8a5
Kibirige Kalule Samuel <samuelkibirigek@gmail.com>
2022-12-19 20:02:08 +0300

    Day 16 turtle with comments

--- turtle_d16.py
+++ turtle_d16.py
@@ -1,13 +1,33 @@
+#First we import the packages or modules
+#If a specific class from a package will be used better to specify as is below
 from turtle import Turtle, Screen
+from prettytable import PrettyTable
 
-timmy = Turtle()
+# timmy = Turtle()
+#
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+"""here we created an object of screen class, 
+used the object to access canvas height attribute and print it out
+also we used exitonclick method to display screen"""
+# my_screen = Screen()
 
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-my_screen = Screen()
-print(my_screen.canvheight)
+"""Installing Pretty table package 
+from the file option in the navbar I went to settings
+from the project drop own went to python interpreter
+searched for PrettyTable package, selected and installed it"""
+table = PrettyTable()
 
-my_screen.exitonclick()
+#I used the add_column method to create the columns
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type",["Electric", "Water", "Fire"])
+
+#Here I use an attribute to set the table content alignment
+table.align = "l"
+print(table)
